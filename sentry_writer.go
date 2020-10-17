4d459c685656e8ca76c72f0c098687567c46181e
Marc Coury <gloriphobia@gmail.com>
2020-10-17 18:16:30 +0100

    Improve documentation (#3)
    * Improve the documentation
    
    * Add sentence about the Flush function
    
    * add comment into package documentation

--- sentry_writer.go
+++ sentry_writer.go
@@ -1,3 +1,36 @@
+/*
+Package sentrywriter is a wrapper around the sentry-go package and implements
+the io.Writer interface. This allows us to send logs from zerolog to Sentry
+(although there is no dependency on zerolog). There is also an in-built
+mechanism to filter log levels, as you usually only want to send error level
+logs to Sentry.
+
+
+Here is a typical example, using zerolog. It is important to defer the
+`sentryWriter.Flush` function because the messages are sent to Sentry
+asynchronously.
+
+    package main
+
+    import (
+	    "github.com/mec07/sentrywriter"
+	    "github.com/rs/zerolog"
+	    "github.com/rs/zerolog/log"
+    )
+
+    func main() {
+	    logLevel := sentrywriter.LogLevel{"error", sentry.LevelError}
+	    sentryWriter, err := sentrywriter.New().WithUserID("userID").WithLogLevel(logLevel).SetDSN("your-project-sentry-dsn")
+	    if err != nil {
+		    log.Error().Err(err).Str("dsn", "your-project-sentry-dsn").Msg("sentrywriter.SentryWriter.SetDSN")
+		    return
+	    }
+	    defer sentryWriter.Flush()
+
+	    consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout}
+	    log.Logger = log.Output(zerolog.MultiLevelWriter(consoleWriter, sentryWriter))
+    }
+*/
 package sentrywriter
 
 import (
